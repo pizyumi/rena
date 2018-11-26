1cfbc2ef9051eee9eac262ab996d231b89360a4d
pizyumi <piz_yumina@hotmail.com>
2018-11-27 00:09:32 +0900

    modify interface

--- asset/js/interface.js
+++ asset/js/interface.js
@@ -81,6 +81,84 @@ function execute_command (context, command) {
 
     return p;
   }
+	else if (cs[0] === 'neg_elim') {
+		var objs = get_n_objects_from_args(context, 2, cs);
+		var p = create_negation_elimination(objs[0], objs[1]);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'neg_intro') {
+		context.dindex++;
+		var objs = get_n_objects_from_args(context, 2, cs);
+		var p = create_negation_introduction(objs[0], objs[1], context.dindex);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'raa') {
+		context.dindex++;
+		var objs = get_n_objects_from_args(context, 2, cs);
+		var p = create_raa(objs[0], objs[1], context.dindex);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'conj_elim_r') {
+		var objs = get_n_objects_from_args(context, 1, cs);
+		var p = create_conjunction_elimination_right(objs[0]);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'conj_elim_l') {
+		var objs = get_n_objects_from_args(context, 1, cs);
+		var p = create_conjunction_elimination_left(objs[0]);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'conj_intro') {
+		var objs = get_n_objects_from_args(context, 2, cs);
+		var p = create_conjunction_introduction(objs[0], objs[1]);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'disj_elim') {
+		context.dindex++;
+		var dindex1 = context.dindex;
+		context.dindex++;
+		var dindex2 = context.dindex;
+		var objs = get_n_objects_from_args(context, 5, cs);
+		var p = create_disjunction_elimination(objs[0], objs[1], objs[2], objs[3], objs[4], dindex1, dindex2);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'disj_intro_r') {
+		var objs = get_n_objects_from_args(context, 2, cs);
+		var p = create_disjunction_introduction_right(objs[0], objs[1]);
+
+		add_object(context, p);
+
+		return p;
+	}
+	else if (cs[0] === 'disj_intro_l') {
+		var objs = get_n_objects_from_args(context, 2, cs);
+		var p = create_disjunction_introduction_left(objs[0], objs[1]);
+
+		add_object(context, p);
+
+		return p;
+	}
   else {
     throw new Error('not supported command.');
   }
